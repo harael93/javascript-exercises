2ddd86dd3d7d991bdb2cdb0d780cfa7902b134bd
harael93 <liquidsoliddesign@gmail.com>
2025-07-09 23:35:27 -0400

    test passed

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  // Convert the string to lowercase and remove non-alphanumeric characters
+  const cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+  let left = 0;
+  let right = cleanedStr.length - 1;
+
+  while (left < right) {
+    if (cleanedStr[left] !== cleanedStr[right]) {
+      return false; // Characters do not match, not a palindrome
+    }
+    left++;
+    right--;
+  }
+
+  return true;
 
 };
 
