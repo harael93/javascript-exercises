c63f6f97bfa46433127c4b44538287540ea8cd97
harael93 <liquidsoliddesign@gmail.com>
2025-07-09 22:42:00 -0400

    test passed

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+function sum(...numbers) {
+  return numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+}
+
+const multiply = function(...numbers) {
+  return numbers.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
 };
 
-const multiply = function() {
+const power = (base, exponent) => Math.pow(base, exponent);
 
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
